a3ad1a0127bd6c80f6d3db7c44cfc942e9490baa
Tima <dadeev.tim@gmail.com>
2022-06-27 11:14:35 +0700

    feat: add function and implement unit test

---
+++ src/02/02.ts
@@ -0,0 +1,55 @@
+export type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+type TechType = {
+    id: number,
+    title: string
+
+}
+export type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+
+export const student: StudentType = {
+    id: 1,
+    name: 'Dimych',
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: 'Surganova 2',
+        city: {
+            title: 'Minsk',
+            countryTitle: 'Belarus'
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: 'HTML'
+        },
+        {
+            id: 2,
+            title: 'CSS'
+        },
+        {
+            id: 3,
+            title: 'REACT'
+        }
+    ]
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
---
+++ src/03/03.ts
@@ -0,0 +1,23 @@
+import {student, StudentType} from "../02/02";
+
+export const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+export function sum2(a: number, b: number) {
+    return a + b;
+}
+
+export const addSkill = (st: StudentType, skill: string) => {
+    st.technologies.push({
+        id: new Date().getTime(),
+        title: skill
+    })
+}
+
+export function addSkill2(st: StudentType, skill: string) {
+    st.technologies.push({
+        id: new Date().getTime(),
+        title: skill
+    })
+}
